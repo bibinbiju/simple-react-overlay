9b5719cccf1ea53d8c788c531cbf500fb677b2d9
Bibin B <bibin.b@accubits.com>
2022-07-12 00:55:47 +0530

    updated

---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "rollup-plugin-typescript2";
+
+const packageJson = require("./package.json");
+
+const data =  {
+    input: "src/lib/index.ts",
+    output: [
+        {
+            file: packageJson.main,
+            format: "cjs",
+            sourcemap: false
+        },
+        // {
+        //     file: packageJson.module,
+        //     format: "esm",
+        //     sourcemap: true
+        // }
+    ],
+    plugins: [
+        peerDepsExternal(),
+        resolve(),
+        commonjs(),
+        typescript({ useTsconfigDeclarationDir: true })
+    ]
+};
+export default data;
